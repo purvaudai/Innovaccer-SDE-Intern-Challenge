ed992ae01fa81d5f813663a6959177c89311cd5f
purvaudai <purvaudai99@gmail.com>
2019-11-28 14:16:29 +0530

    Changed start.sh

--- Entry_Management/start.sh
+++ Entry_Management/start.sh
@@ -2,6 +2,6 @@
 
 # Start Gunicorn processes
 source venv/bin/activate
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 source variables.env
-python Entry_Management_App/manage.py runserver
+python3 Entry_Management_App/manage.py runserver
